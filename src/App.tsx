f558f8a3fd4806e4bf7684c72178a6c6354ec419
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-10-06 10:17:44 -0600

    add a botton to signout and rendering in order the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import Auth from "./components/Auth/Auth";
 import Cookies from "universal-cookie";
 import Chat from "./components/Chat/Chat";
+import {signOut} from "firebase/auth"
+import { auth } from "./components/fireBaseConfig/fireBaseConfig";
+
 const cookies = new Cookies();
 
 const App = () => {
   const [isAuth, setIsAuth] = useState<boolean>(cookies.get("auth-token") ? true : false);
   const [room, setRoom] = useState<string | null>(null);
   const roomInputRef = useRef<HTMLInputElement | null>(null);
+
+ const signUserOut = async (): Promise<void> => {
+  try {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  } catch (error) {
+    console.error("Error al cerrar sesión:", error);
+  }
+};
 
   if (!isAuth) {
     return (
@@ -18,6 +32,7 @@ const App = () => {
   }
 
   return (
+    <>
     <div>
       {room ? (
         <Chat room={room}></Chat>
@@ -30,6 +45,12 @@ const App = () => {
         </div>
       )}
     </div>
+
+      <div>
+        <button onClick={signUserOut}>Sign out</button>
+      </div>
+
+    </>
   );
 };
 
