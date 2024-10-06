96224aebb9ce042480b77e9b2902132274ac8f5d
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-10-06 08:23:03 -0600

    feat(App.tsx) : creating authentification process

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import Auth from "./components/Auth/Auth";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [isAuth, setIsAuth] = useState<boolean>(cookies.get("auth-token") ? true : false);
+  const [room, setRoom] = useState<string | null>(null);
+  const roomInputRef = useRef<HTMLInputElement | null>(null);
 
-export default App
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth setIsAuth={setIsAuth}></Auth>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label>Enter Room Name: </label> <input ref={roomInputRef} />{" "}
+          <button onClick={() => setRoom(roomInputRef.current!.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
